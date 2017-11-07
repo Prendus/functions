3f1df2f386644e661f15609d1d9d5526bad74dbe
Jordan Last <jordan.michael.last@gmail.com>
2017-11-07 13:55:21 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,14 @@
+// returns a random integer between min (included) and max (included)
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+// returns a random float between min (included) and max (included)
+function randFloat(min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
+
+// returns a random item from the list of items passed in
+function randItem(items) {
+  return items[randInt(0, items.length - 1)];
+}
