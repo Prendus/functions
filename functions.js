30085b16052cac414c242a14fba73f16ef2ab42e
Jordan Last <jordan.michael.last@gmail.com>
2017-11-09 14:09:46 -0700

    Update functions.js

--- functions.js
+++ functions.js
@@ -12,3 +12,8 @@ function randFloat(min, max) {
 function randItem(items) {
   return items[randInt(0, items.length - 1)];
 }
+
+// returns the number passed in with a precision number of digits after the decimal point
+function toPrecision(number, precision) {
+    return +((number).toFixed());
+}
